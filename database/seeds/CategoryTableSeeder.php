50eadbc11f92a3adfb13b0d7d01493acd45dfe08
Tiago Alves <tcristianoalves@outlook.com>
2016-01-11 01:06:09 +0000

    Migração de produtos

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -12,6 +12,6 @@ class CategoryTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Category::class,1000)->create();
+        factory(Category::class,10)->create();
     }
 }
